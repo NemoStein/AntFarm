aa8217bff03ec918a6e62b9a7a0b04fb87fc87ed
NemoStein <contato@nemostein.com.br>
2019-02-25 17:29:22 -0300

    Inital formicary definition

---
+++ src/ann/Formicary.js
@@ -0,0 +1,33 @@
+export default class Formicary
+{
+	/**
+	 * @param {Number} width Width of the formicary
+	 * @param {Number} height Height of the formicary
+	 * @param {Number} food Food count available
+	 * @param {Number} anthillX X position of the anthill
+	 * @param {Number} anthillY Y position of the anthill
+	 */
+	constructor(width, height, food, anthillX, anthillY)
+	{
+		this.width = width
+		this.height = height
+		this.food = food
+		this.anthillX = anthillX
+		this.anthillY = anthillY
+	}
+	
+	getFoodScentFrom(x, y)
+	{
+		
+	}
+	
+	getTraceScentFrom(x, y)
+	{
+		
+	}
+	
+	getAnthillDirectionFrom(x, y)
+	{
+		
+	}
+}
